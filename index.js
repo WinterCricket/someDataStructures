79bcd962ac509dcfb5fa1c1dec3da3508ed67335
WinterCricket <hodhodhod2003@gmail.com>
2019-03-12 22:46:56 -0500

    add to linkedlist class getAt method

--- index.js
+++ index.js
@@ -75,6 +75,21 @@ class LinkedList{
   		this.head = new Node(data);
   	}
   }
+  getAt(index){
+
+		let counter = 0;
+		let node = this.head;
+		while(node){
+
+			if(counter === index){
+				return node;
+			}
+			counter++;
+			node = node.next;
+		}
+		return null;
+		
+	}
 
 }
 //  const nodeOne = new Node(33);
